d73474910b183e7988497fe5636c3dbd5161e48f
Ken Chen <kenchen@microsoft.com>
2018-09-12 14:49:58 +0800

    Fix CORS issue

--- samples/simple-chat/js/functionapp/messages/index.js
+++ samples/simple-chat/js/functionapp/messages/index.js
@@ -2,9 +2,21 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 module.exports = function (context, req) {
-  context.bindings.signalRMessages = [{
-    "target": "newMessage",
-    "arguments": [ req.body ]
-  }];
+  // Azure function doesn't support CORS well, workaround it by explicitly return CORS headers
+  context.res = {
+    headers: {
+      'Access-Control-Allow-Credentials': 'true',
+      'Access-Control-Allow-Origin': req.headers.origin,
+      'Access-Control-Allow-Headers': req.headers['access-control-request-headers']
+    }
+  };
+
+  if (req.method === 'POST') {
+    context.bindings.signalRMessages = [{
+      "target": "newMessage",
+      "arguments": [req.body]
+    }];
+  }
+
   context.done();
 };
