a4d26ba3fd7eada4a70fb02cbbb864a0958cf1bf
Wanpeng Li <wanl@microsoft.com>
2019-05-08 11:32:12 +0800

    update auth chat sample and add readme
    1. add proxies.json
    2. add readme to show how to deploy on azure
    3. fix bug in auth

--- samples/chat-with-auth/content/scripts/auth.js
+++ samples/chat-with-auth/content/scripts/auth.js
@@ -1,6 +1,8 @@
-(function () {
-  let authToken = ""
+(function() {
+  window.authProvider = 'aad'; // aad, microsoftaccount, google, facebook, twitter
+  window.apiBaseUrl = 'https://<your function name>.azurewebsites.net';
 
+  let authToken = ""
   if (window.location.hash) {
     const match = window.location.hash.match(/\btoken=([^&]+)/)
     if (match && match[1]) {
@@ -9,7 +11,7 @@
       history.pushState("", document.title, window.location.pathname + window.location.search)
     }
   }
-  
+
   if (!authToken) {
     authToken = sessionStorage.getItem('authToken')
   }
@@ -17,7 +19,7 @@
   window.auth = {
     token: authToken,
     loginUrl: window.apiBaseUrl +
-      '/.auth/login/twitter?session_mode=token&post_login_redirect_url=' +
+      `/.auth/login/${window.authProvider}?session_mode=token&post_login_redirect_url=` +
       encodeURIComponent(window.location.href),
     logout: function() {
       sessionStorage.removeItem('authToken')
