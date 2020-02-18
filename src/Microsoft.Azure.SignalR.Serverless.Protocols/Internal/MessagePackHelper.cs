4813c5710c0f7ebff381b10c214b081e0bb05930
Chenyang Liu <chenyl@microsoft.com>
2020-02-18 10:39:22 +0800

    Revert "Use messagepack 2.0"
    This reverts commit 584fec8efbc9e7cce321fa10a0a81800d488ac79.

--- src/Microsoft.Azure.SignalR.Serverless.Protocols/Internal/MessagePackHelper.cs
+++ src/Microsoft.Azure.SignalR.Serverless.Protocols/Internal/MessagePackHelper.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Buffers;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -12,73 +11,186 @@ namespace Microsoft.Azure.SignalR.Serverless.Protocols
 {
     internal class MessagePackHelper
     {
-        public static void SkipHeaders(ref MessagePackReader reader)
+        private static InvocationMessage ConvertInvocationMessage(byte[] input, ref int offset)
         {
-            var headerCount = ReadMapLength(ref reader, "headers");
+            var invocationMessage = new InvocationMessage()
+            {
+                Type = ServerlessProtocolConstants.InvocationMessageType,
+            };
+
+            SkipHeaders(input, ref offset);
+            invocationMessage.InvocationId = ReadInvocationId(input, ref offset);
+            invocationMessage.Target = ReadTarget(input, ref offset);
+            invocationMessage.Arguments = ReadArguments(input, ref offset);
+            return invocationMessage;
+        }
+
+        public static void SkipHeaders(byte[] input, ref int offset)
+        {
+            var headerCount = ReadMapLength(input, ref offset, "headers");
             if (headerCount > 0)
             {
                 for (var i = 0; i < headerCount; i++)
                 {
-                    ReadString(ref reader, $"headers[{i}].Key");
-                    ReadString(ref reader, $"headers[{i}].Value");
+                    ReadString(input, ref offset, $"headers[{i}].Key");
+                    ReadString(input, ref offset, $"headers[{i}].Value");
                 }
             }
         }
 
-        public static string ReadInvocationId(ref MessagePackReader reader)
+        public static string ReadInvocationId(byte[] input, ref int offset)
         {
-            return ReadString(ref reader, "invocationId");
+            return ReadString(input, ref offset, "invocationId");
         }
 
-        public static string ReadTarget(ref MessagePackReader reader)
+        public static string ReadTarget(byte[] input, ref int offset)
         {
-            return ReadString(ref reader, "target");
+            return ReadString(input, ref offset, "target");
         }
 
-        public static object[] ReadArguments(ref MessagePackReader reader)
+        public static object[] ReadArguments(byte[] input, ref int offset)
         {
-            var argumentCount = ReadArrayLength(ref reader, "arguments");
+            var argumentCount = ReadArrayLength(input, ref offset, "arguments");
             var array = new object[argumentCount];
             for (int i = 0; i < argumentCount; i++)
             {
-                array[i] = ReadObject(ref reader);
+                array[i] = ReadObject(input, ref offset);
             }
             return array;
         }
 
-        public static object ReadObject(ref MessagePackReader reader)
+        public static int ReadInt32(byte[] input, ref int offset, string field)
         {
-            var type = reader.NextMessagePackType;
+            Exception msgPackException = null;
+            try
+            {
+                var readInt = MessagePackBinary.ReadInt32(input, offset, out var readSize);
+                offset += readSize;
+                return readInt;
+            }
+            catch (Exception e)
+            {
+                msgPackException = e;
+            }
+
+            throw new InvalidDataException($"Reading '{field}' as Int32 failed.", msgPackException);
+        }
+
+        public static string ReadString(byte[] input, ref int offset, string field)
+        {
+            Exception msgPackException = null;
+            try
+            {
+                var readString = MessagePackBinary.ReadString(input, offset, out var readSize);
+                offset += readSize;
+                return readString;
+            }
+            catch (Exception e)
+            {
+                msgPackException = e;
+            }
+
+            throw new InvalidDataException($"Reading '{field}' as String failed.", msgPackException);
+        }
+
+        public static bool ReadBoolean(byte[] input, ref int offset, string field)
+        {
+            Exception msgPackException = null;
+            try
+            {
+                var readBool = MessagePackBinary.ReadBoolean(input, offset, out var readSize);
+                offset += readSize;
+                return readBool;
+            }
+            catch (Exception e)
+            {
+                msgPackException = e;
+            }
+
+            throw new InvalidDataException($"Reading '{field}' as Boolean failed.", msgPackException);
+        }
+
+        public static long ReadMapLength(byte[] input, ref int offset, string field)
+        {
+            Exception msgPackException = null;
+            try
+            {
+                var readMap = MessagePackBinary.ReadMapHeader(input, offset, out var readSize);
+                offset += readSize;
+                return readMap;
+            }
+            catch (Exception e)
+            {
+                msgPackException = e;
+            }
+
+            throw new InvalidDataException($"Reading map length for '{field}' failed.", msgPackException);
+        }
+
+        public static long ReadArrayLength(byte[] input, ref int offset, string field)
+        {
+            Exception msgPackException = null;
+            try
+            {
+                var readArray = MessagePackBinary.ReadArrayHeader(input, offset, out var readSize);
+                offset += readSize;
+                return readArray;
+            }
+            catch (Exception e)
+            {
+                msgPackException = e;
+            }
+
+            throw new InvalidDataException($"Reading array length for '{field}' failed.", msgPackException);
+        }
+
+        public static object ReadObject(byte[] input, ref int offset)
+        {
+            var type = MessagePackBinary.GetMessagePackType(input, offset);
+            int size;
             switch (type)
             {
                 case MessagePackType.Integer:
-                    return reader.ReadInt64();
+                    var intValue = MessagePackBinary.ReadInt64(input, offset, out size);
+                    offset += size;
+                    return intValue;
                 case MessagePackType.Nil:
-                    reader.ReadNil();
+                    MessagePackBinary.ReadNil(input, offset, out size);
+                    offset += size;
                     return null;
                 case MessagePackType.Boolean:
-                    return reader.ReadBoolean();
+                    var boolValue = MessagePackBinary.ReadBoolean(input, offset, out size);
+                    offset += size;
+                    return boolValue;
                 case MessagePackType.Float:
-                    return reader.ReadDouble();
+                    var doubleValue = MessagePackBinary.ReadDouble(input, offset, out size);
+                    offset += size;
+                    return doubleValue;
                 case MessagePackType.String:
-                    return reader.ReadString();
+                    var textValue = MessagePackBinary.ReadString(input, offset, out size);
+                    offset += size;
+                    return textValue;
                 case MessagePackType.Binary:
-                    return reader.ReadBytes()?.ToArray() ?? Array.Empty<byte>();
+                    var binaryValue = MessagePackBinary.ReadBytes(input, offset, out size);
+                    offset += size;
+                    return binaryValue;
                 case MessagePackType.Array:
-                    var argumentCount = ReadArrayLength(ref reader, "arguments");
+                    var argumentCount = ReadArrayLength(input, ref offset, "arguments");
                     var array = new object[argumentCount];
                     for (int i = 0; i < argumentCount; i++)
                     {
-                        array[i] = ReadObject(ref reader);
+                        array[i] = ReadObject(input, ref offset);
                     }
                     return array;
                 case MessagePackType.Map:
-                    var propertyCount = reader.ReadMapHeader();
+                    var propertyCount = MessagePackBinary.ReadMapHeader(input, offset, out size);
+                    offset += size;
                     var map = new Dictionary<string, object>();
                     for (int i = 0; i < propertyCount; i++)
                     {
-                        var textValue = reader.ReadString();
-                        var value = ReadObject(ref reader);
+                        textValue = MessagePackBinary.ReadString(input, offset, out size);
+                        offset += size;
+                        var value = ReadObject(input, ref offset);
                         map[textValue] = value;
                     }
                     return map;
@@ -86,85 +198,6 @@ namespace Microsoft.Azure.SignalR.Serverless.Protocols
                 case MessagePackType.Unknown:
                 default:
                     return null;
-            }
-        }
-
-        public static int ReadInt32(ref MessagePackReader reader, string field)
-        {
-            try
-            {
-                return reader.ReadInt32();
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidDataException($"Reading '{field}' as Int32 failed.", ex);
-            }
-
-        }
-
-        private static string ReadString(ref MessagePackReader reader, string field)
-        {
-            try
-            {
-                return reader.ReadString();
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidDataException($"Reading '{field}' as String failed.", ex);
-            }
-        }
-
-        private static string[] ReadStringArray(ref MessagePackReader reader, string field)
-        {
-            var arrayLength = ReadArrayLength(ref reader, field);
-            if (arrayLength > 0)
-            {
-                var array = new string[arrayLength];
-                for (int i = 0; i < arrayLength; i++)
-                {
-                    array[i] = ReadString(ref reader, $"{field}[{i}]");
-                }
-
-                return array;
-            }
-
-            return null;
-        }
-
-        private static byte[] ReadBytes(ref MessagePackReader reader, string field)
-        {
-            try
-            {
-                return reader.ReadBytes()?.ToArray() ?? Array.Empty<byte>();
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidDataException($"Reading '{field}' as Byte[] failed.", ex);
-            }
-
-        }
-
-        private static long ReadMapLength(ref MessagePackReader reader, string field)
-        {
-            try
-            {
-                return reader.ReadMapHeader();
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidDataException($"Reading map length for '{field}' failed.", ex);
-            }
-        }
-
-        private static long ReadArrayLength(ref MessagePackReader reader, string field)
-        {
-            try
-            {
-                return reader.ReadArrayHeader();
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidDataException($"Reading array length for '{field}' failed.", ex);
             }
         }
     }
--- src/Microsoft.Azure.SignalR.Serverless.Protocols/MessagePackServerlessProtocol.cs
+++ src/Microsoft.Azure.SignalR.Serverless.Protocols/MessagePackServerlessProtocol.cs
@@ -1,7 +1,10 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Buffers;
+using System.Collections.Generic;
+using System.IO;
 
 using MessagePack;
 
@@ -13,13 +16,15 @@ namespace Microsoft.Azure.SignalR.Serverless.Protocols
 
         public bool TryParseMessage(ref ReadOnlySequence<byte> input, out ServerlessMessage message)
         {
-            var reader = new MessagePackReader(input);
-            _ = reader.ReadArrayHeader();
-            var messageType = MessagePackHelper.ReadInt32(ref reader, "messageType");
+            var array = input.ToArray();
+            var startOffset = 0;
+            _ = MessagePackBinary.ReadArrayHeader(array, startOffset, out var readSize);
+            startOffset += readSize;
+            var messageType = MessagePackHelper.ReadInt32(array, ref startOffset, "messageType");
             switch (messageType)
             {
                 case ServerlessProtocolConstants.InvocationMessageType:
-                    message = ConvertInvocationMessage(ref reader);
+                    message = ConvertInvocationMessage(array, ref startOffset);
                     break;
                 default:
                     // TODO:OpenConnectionMessage and CloseConnectionMessage only will be sent in JSON format. It can be added later.
@@ -30,17 +35,17 @@ namespace Microsoft.Azure.SignalR.Serverless.Protocols
             return message != null;
         }
 
-        private static InvocationMessage ConvertInvocationMessage(ref MessagePackReader reader)
+        private static InvocationMessage ConvertInvocationMessage(byte[] input, ref int offset)
         {
             var invocationMessage = new InvocationMessage()
             {
                 Type = ServerlessProtocolConstants.InvocationMessageType,
             };
 
-            MessagePackHelper.SkipHeaders(ref reader);
-            invocationMessage.InvocationId = MessagePackHelper.ReadInvocationId(ref reader);
-            invocationMessage.Target = MessagePackHelper.ReadTarget(ref reader);
-            invocationMessage.Arguments = MessagePackHelper.ReadArguments(ref reader);
+            MessagePackHelper.SkipHeaders(input, ref offset);
+            invocationMessage.InvocationId = MessagePackHelper.ReadInvocationId(input, ref offset);
+            invocationMessage.Target = MessagePackHelper.ReadTarget(input, ref offset);
+            invocationMessage.Arguments = MessagePackHelper.ReadArguments(input, ref offset);
             return invocationMessage;
         }
     }
