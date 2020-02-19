2b5c185299a4bb5513bca51c5746707a8808565c
Chenyang Liu <chenyl@microsoft.com>
2020-02-19 16:17:33 +0800

    Add trigger binding

---
+++ src/SignalRServiceExtension/TriggerBindings/SignalRTriggerAttribute.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+using Microsoft.Azure.WebJobs.Description;
+
+namespace Microsoft.Azure.WebJobs.Extensions.SignalRService
+{
+    [AttributeUsage(AttributeTargets.ReturnValue | AttributeTargets.Parameter)]
+    [Binding]
+    public class SignalRTriggerAttribute : Attribute
+    {
+        /// <summary>
+        /// THe hub of request belongs to.
+        /// </summary>
+        [AutoResolve]
+        public string HubName { get; set; }
+
+        /// <summary>
+        /// The event of the request.
+        /// </summary>
+        public string Event { get; set; }
+
+        /// <summary>
+        /// Two optional value: connections and messages
+        /// </summary>
+        public string Category { get; set; }
+
+        /// <summary>
+        /// Used for messages category. All the name defined in <see cref="ParameterNames"/> will map to
+        /// Arguments in InvocationMessage by order. And the these name can be used in parameters of method
+        /// directly.
+        /// </summary>
+        public string[] ParameterNames { get; set; }
+    }
+}
