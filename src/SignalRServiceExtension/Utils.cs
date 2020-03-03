20dc682b8f33e3bed3468f69207295d9d48e375a
Unknown <wanl@microsoft.com>
2020-03-03 11:27:43 +0800

    refactor

---
+++ src/SignalRServiceExtension/Utils.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Linq;
+
+namespace Microsoft.Azure.WebJobs.Extensions.SignalRService
+{
+    internal class Utils
+    {
+        public static string FirstOrDefault(params string[] values)
+        {
+            return values.FirstOrDefault(v => !string.IsNullOrEmpty(v));
+        }
+
+        public static AzureSignalRClient GetAzureSignalRClient(string attributeConnectionString, string attributeHubName, SignalROptions options)
+        {
+            var connectionString = FirstOrDefault(attributeConnectionString, options.ConnectionString);
+            var hubName = FirstOrDefault(attributeHubName, options.HubName);
+
+            return new AzureSignalRClient(StaticServiceHubContextStore.ServiceManagerStore, connectionString, hubName);
+        }
+    }
+}
