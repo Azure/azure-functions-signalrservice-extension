8610458e7517450ff52f9c496f41eef3fb940b21
Chenyang Liu <chenyl@microsoft.com>
2020-09-29 14:24:35 +0800

    Add a new ServerlessHub constructor for unit test purpose (#138)
    * Add a constructor for ServerlessHub which can be used in ut
    
    * Minor fix
    
    * Revert new language character

---
+++ test/SignalRServiceExtension.Tests/Trigger/ServerlessHubTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Azure.SignalR.Management;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Moq;
+using Xunit;
+
+namespace SignalRServiceExtension.Tests.Trigger
+{
+    public class ServerlessHubTest
+    {
+        [Fact]
+        private async Task ServerlessHubUnitTest()
+        {
+            var clientProxyMoc = new Mock<IClientProxy>();
+            clientProxyMoc
+                .Setup(c => c.SendCoreAsync(It.IsAny<string>(), It.IsAny<object[]>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.CompletedTask);
+            var serviceHubContextMoc = new Mock<IServiceHubContext>();
+            var hubClientsMoc = new Mock<IHubClients>();
+            hubClientsMoc.SetupGet(h => h.All).Returns(clientProxyMoc.Object);
+            serviceHubContextMoc.SetupGet(s => s.Clients).Returns(hubClientsMoc.Object);
+
+            var serviceManagerMoc = new Mock<IServiceManager>();
+            var myHub = new MyHub(serviceHubContextMoc.Object, serviceManagerMoc.Object);
+
+            // Unit test negotiate
+            var userId = Guid.NewGuid().ToString();
+            myHub.Negotiate(userId);
+            serviceManagerMoc.Verify(s => s.GetClientEndpoint(nameof(MyHub)), Times.Once);
+            serviceManagerMoc.Verify(s => s.GenerateClientAccessToken(nameof(MyHub), userId, It.IsAny<IList<Claim>>(), It.IsAny<TimeSpan?>()), Times.Once);
+
+            // Unit test broadcast method
+            var target = Guid.NewGuid().ToString();
+            var message = Guid.NewGuid().ToString();
+            await myHub.Broadcast(new InvocationContext(), target, message);
+            clientProxyMoc.Verify(c => c.SendCoreAsync(target, It.IsAny<object[]>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
+    }
+
+    public class MyHub : ServerlessHub
+    {
+        // Use default value = null to reconcile testing and production purpose.
+        public MyHub(IServiceHubContext serviceHubContext = null, IServiceManager serviceManager = null): base(serviceHubContext, serviceManager)
+        {
+        }
+
+        [FunctionName("negotiate")]
+        public SignalRConnectionInfo Negotiate(string userId)
+        {
+            return base.Negotiate(userId);
+        }
+
+        [FunctionName(nameof(Broadcast))]
+        public async Task Broadcast([SignalRTrigger] InvocationContext invocationContext, string target, string message)
+        {
+            await Clients.All.SendAsync(target, message);
+        }
+    }
+}
