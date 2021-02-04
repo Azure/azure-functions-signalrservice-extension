53a352afc2ba92fe8cfafe70bd1dc11a8c2b325a
Zitong Yang <zityang@microsoft.com>
2021-02-04 14:43:38 +0800

    Add multiple endpoints JS sample

--- samples/simple-chat/js/functionapp/messages/index.js
+++ samples/simple-chat/js/functionapp/messages/index.js
@@ -1,12 +1,13 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-module.exports = function (context, req) {
+module.exports = function (context, req, endpoints) {
   context.bindings.signalRMessages = [{
     "userId": req.body.recipient,
     "groupName": req.body.groupname,
     "target": "newMessage",
-    "arguments": [ req.body ]
+    "arguments": [req.body],
+    "endpoints": endpoints.filter(endpoint => endpoint.name !== "") // if this field is not set, send to all endpoints
   }];
   context.done();
 };
