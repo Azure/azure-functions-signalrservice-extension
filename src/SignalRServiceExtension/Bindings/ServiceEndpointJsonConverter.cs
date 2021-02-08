0b4bd0c4a8f9fc36cb9da55ced02519e13dec218
Zitong Yang <zityang@microsoft.com>
2021-02-08 17:00:18 +0800

    Move service endpoint to lite service endpoint conversion into converter

--- src/SignalRServiceExtension/Bindings/ServiceEndpointJsonConverter.cs
+++ src/SignalRServiceExtension/Bindings/ServiceEndpointJsonConverter.cs
@@ -9,14 +9,33 @@ namespace Microsoft.Azure.WebJobs.Extensions.SignalRService
 {
     internal class ServiceEndpointJsonConverter : JsonConverter<ServiceEndpoint>
     {
+        private const string FakeAccessKey = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
         public override ServiceEndpoint ReadJson(JsonReader reader, Type objectType, ServiceEndpoint existingValue, bool hasExistingValue, JsonSerializer serializer)
         {
-            return serializer.Deserialize<LiteServiceEndpoint>(reader).ToEqualServiceEndpoint();
+            return ToEqualServiceEndpoint(serializer.Deserialize<LiteServiceEndpoint>(reader));
         }
 
         public override void WriteJson(JsonWriter writer, ServiceEndpoint value, JsonSerializer serializer)
         {
-            serializer.Serialize(writer, value.ToLiteServiceEndpoint());
+            serializer.Serialize(writer, ToLiteServiceEndpoint(value));
+        }
+
+        private LiteServiceEndpoint ToLiteServiceEndpoint(ServiceEndpoint e)
+        {
+            return new LiteServiceEndpoint
+            {
+                EndpointType = e.EndpointType,
+                Name = e.Name,
+                Endpoint = e.Endpoint,
+                Online = e.Online
+            };
+        }
+
+        private ServiceEndpoint ToEqualServiceEndpoint(LiteServiceEndpoint e)
+        {
+            var connectionString = $"Endpoint={e.Endpoint};AccessKey={FakeAccessKey};Version=1.0;";
+            return new ServiceEndpoint(connectionString, e.EndpointType, e.Name);
         }
     }
 }
