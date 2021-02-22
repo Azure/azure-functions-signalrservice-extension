4385ffdf72b5c06537c6e1c99f3ef3fec6623f9d
Zitong Yang <zityang@microsoft.com>
2021-02-22 18:47:53 +0800

    Split extensions src TFM into 2.0 and 3.1

--- test/SignalRServiceExtension.Tests/DefaultSecurityTokenValidatorTests.cs
+++ test/SignalRServiceExtension.Tests/DefaultSecurityTokenValidatorTests.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Text;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Extensions.SignalRService;
 using Microsoft.Extensions.Primitives;
