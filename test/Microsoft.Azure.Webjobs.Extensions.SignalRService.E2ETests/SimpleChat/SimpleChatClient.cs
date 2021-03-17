d6bdff0bcdea432a64f47095b7faa245559890de
yzt <zityang@microsoft.com>
2021-03-17 13:14:33 +0800

    Add E2E test for send, group management (#178)
    * Add E2E test for send, group management
    
    * add local.settings.json to .gitignore
    
    * Add .Net 3.1.x,   .Net 2.1.x  to Ubuntu environment

---
+++ test/Microsoft.Azure.Webjobs.Extensions.SignalRService.E2ETests/SimpleChat/SimpleChatClient.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+
+namespace Microsoft.Azure.Webjobs.Extensions.SignalRService.E2ETests
+{
+    public class SimpleChatClient
+    {
+        private static readonly HttpClient HttpClient = new();
+
+        public static async Task<SignalRConnectionInfo> Negotiate(string userId, string url)
+        {
+            const string path = "negotiate";
+            var request = new HttpRequestMessage(HttpMethod.Post, $"{url}/api/{path}?userid={userId}");
+            var responseMessage = await HttpClient.SendAsync(request);
+
+            var connectionInfo = await responseMessage.Content.ReadFromJsonAsync<SignalRConnectionInfo>();
+            return connectionInfo;
+        }
+
+        public static Task Send(string url, SignalRMessage message)
+        {
+            const string SendPath = "send";
+            var content = JsonContent.Create(message);
+            return HttpClient.PostAsync($"{url}/api/{SendPath}", content);
+        }
+
+        public static Task Group(string url,SignalRGroupAction action)
+        {
+            const string GroupPath = "group";
+            var content = JsonContent.Create(action);
+            return HttpClient.PostAsync($"{url}/api/{GroupPath}", content);
+        }
+    }
+}
---
+++ test/Microsoft.Azure.Webjobs.Extensions.SignalRService.E2ETests/SimpleChat/SkipIfSimpleChatFunctionAbsentAttribute.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Testing.xunit;
+using static Microsoft.Azure.Webjobs.Extensions.SignalRService.E2ETests.SimpleChatTests;
+
+namespace Microsoft.Azure.Webjobs.Extensions.SignalRService.E2ETests.SimpleChat
+{
+    public class SkipIfSimpleChatFunctionAbsentAttribute : Attribute, ITestCondition
+    {
+        public bool IsMet => BaseUrls.Data.Count() == 0;
+
+        public string SkipReason => "Simple-chat functions base urls are not configured.";
+    }
+}
