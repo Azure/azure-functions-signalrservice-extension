ed9232397994d4dbd42a7144e6d83ad1ec9f536f
Zitong Yang <zityang@microsoft.com>
2021-03-26 17:54:45 +0800

    Refactor AzureSignalRClient

---
+++ src/SignalRServiceExtension/Bindings/SignalRAsyncCollectorBuilder.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.WebJobs.Extensions.SignalRService
+{
+    internal class SignalRAsyncCollectorBuilder<T> : IAsyncConverter<SignalRAttribute, IAsyncCollector<T>>
+    {
+        public async Task<IAsyncCollector<T>> ConvertAsync(SignalRAttribute input, CancellationToken cancellationToken)
+        {
+            var client = await Utils.GetAzureSignalRClient(input.ConnectionStringSetting, input.HubName);
+            return new SignalRAsyncCollector<T>(client);
+        }
+    }
+}
