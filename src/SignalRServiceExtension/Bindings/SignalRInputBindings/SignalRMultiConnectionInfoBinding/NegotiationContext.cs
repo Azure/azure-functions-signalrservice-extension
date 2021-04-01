9692f5e4708aad29331e2a672d5af8c363dce83d
Zitong Yang <zityang@microsoft.com>
2021-04-01 16:14:38 +0800

    fix

--- src/SignalRServiceExtension/Bindings/SignalRInputBindings/SignalRMultiConnectionInfoBinding/NegotiationContext.cs
+++ src/SignalRServiceExtension/Bindings/SignalRInputBindings/SignalRMultiConnectionInfoBinding/NegotiationContext.cs
@@ -2,11 +2,10 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 
 namespace Microsoft.Azure.WebJobs.Extensions.SignalRService
 {
-    [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    [JsonObject]
     internal class NegotiationContext
     {
         public EndpointConnectionInfo[] Endpoints { get; set; }
