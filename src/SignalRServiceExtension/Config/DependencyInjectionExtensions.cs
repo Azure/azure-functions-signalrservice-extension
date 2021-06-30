4519f4574a51befb7c1db0883ecde570c4265884
Zitong Yang <zityang@microsoft.com>
2021-06-30 14:02:37 +0800

    Modify DI logic
    If user want NewtonsoftCamelCase, no need set “HubProtocol=NewtonsoftJson". "HubProtocol:NewtonsoftJson:CamelCase=true" implies Newtonsoft already.

--- src/SignalRServiceExtension/Config/DependencyInjectionExtensions.cs
+++ src/SignalRServiceExtension/Config/DependencyInjectionExtensions.cs
@@ -16,29 +16,26 @@ namespace Microsoft.Azure.WebJobs.Extensions.SignalRService
         public static IServiceCollection SetHubProtocol(this IServiceCollection services, IConfiguration configuration)
         {
             var hubProtocolConfig = configuration[Constants.AzureSignalRHubProtocol];
-            if (hubProtocolConfig is not null)
+            if (hubProtocolConfig is not null && Environment.Version.Major == 4)
             {
-                if (Environment.Version.Major == 4)
-                {
-                    // .Net Core 2.x is always Newtonsoft.Json.
-                    throw new InvalidOperationException(HubProtocolError);
-                }
-                var hubProtocol = Enum.Parse<HubProtocol>(hubProtocolConfig);
-                //If hubProtocolConfig is SystemTextJson for .Net Core 3.1, do nothing, as transient mode doesn't accept it and persisent mode is already System.Text.Json by default.
-                if (!DotnetRuntime(configuration) || hubProtocol == HubProtocol.NewtonsoftJson)
-                {
-                    if (configuration.GetValue(Constants.AzureSignalRNewtonsoftCamelCase, false))
-                    {
-                        // The default options is camelCase.
-                        services.AddNewtonsoftHubProtocol(o => { });
-                    }
-                    else
-                    {
-                        // Reset the options to keep backward compatibility.
-                        services.AddNewtonsoftHubProtocol(o => o.PayloadSerializerSettings = new JsonSerializerSettings());
-                    }
-                }
+                // .Net Core 2.x is always Newtonsoft.Json.
+                throw new InvalidOperationException(HubProtocolError);
             }
+            
+            //indicates Newtonsoft, camcelCase
+            if (configuration.GetValue(Constants.AzureSignalRNewtonsoftCamelCase, false))
+            {
+                // The default options is camelCase.
+                return services.AddNewtonsoftHubProtocol(o => { });
+            }
+            
+            if (!DotnetRuntime(configuration) || Enum.TryParse<HubProtocol>(hubProtocolConfig, out var result) && result == HubProtocol.NewtonsoftJson)
+            {
+                // Reset the options to keep backward compatibility.
+                return services.AddNewtonsoftHubProtocol(o => o.PayloadSerializerSettings = new JsonSerializerSettings());
+            }
+            
+            //If hubProtocolConfig is SystemTextJson for .Net Core 3.1, do nothing, as transient mode doesn't accept it and persisent mode is already System.Text.Json by default.
             return services;
         }
 
