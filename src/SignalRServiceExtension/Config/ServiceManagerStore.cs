e51df452f1af8fe2e79128eac64b64abec73a60e
Brennan Bugbee <Bugbeeb@gmail.com>
2021-08-17 09:13:09 -0400

    Revert "unnecessary usings"
    This reverts commit c20e59cd97cef7e39140c80f955126b8bd8246b8.

--- src/SignalRServiceExtension/Config/ServiceManagerStore.cs
+++ src/SignalRServiceExtension/Config/ServiceManagerStore.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using Microsoft.Azure.SignalR;
 using Microsoft.Azure.SignalR.Management;
 using Microsoft.Extensions.Configuration;
