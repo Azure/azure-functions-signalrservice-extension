3d3aba3d10a404626193e89df19d7aab310ddbac
Zitong Yang <zityang@microsoft.com>
2021-09-03 16:30:09 +0800

    Move identity-based service endpoint parsing methods here
    The code won't exist in SDK anymore. Also move the tests here

--- src/SignalRServiceExtension/Config/IConfigurationExtensions.cs
+++ src/SignalRServiceExtension/Config/IConfigurationExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Azure.Core;
 using Microsoft.Azure.SignalR;
 using Microsoft.Extensions.Azure;
@@ -11,10 +10,6 @@ namespace Microsoft.Azure.WebJobs.Extensions.SignalRService
     /// <summary>
     /// A helper class to parse <see cref="ServiceEndpoint"/> from configuration.
     /// </summary>
-    /// <remarks>
-    /// Copyied from https://github.com/Azure/azure-signalr/blob/e5a69b20daf7f7aa2786ff59df61433c4050c6ab/src/Microsoft.Azure.SignalR.Common/Endpoints/IConfigurationExtension.cs.
-    /// Although we have friend access to that internal class, referencing internal methods are not recommended as changes of internal methods might break this package.
-    /// </remarks>
     internal static class IConfigurationExtensions
     {
         public static IEnumerable<ServiceEndpoint> GetEndpoints(this IConfiguration config, AzureComponentFactory azureComponentFactory)
